e1b20a66e25b5903a4c20eb144cac95747ed2c0b
Bryce <bryce.schafer@ascte.org>
2025-09-16 13:16:08 -0500

    Update OLED-testing.py

--- module testing/OLED-testing.py
+++ module testing/OLED-testing.py
@@ -7,21 +7,32 @@ from xglcd_font import XglcdFont
 
 """Test code."""
 bally = XglcdFont('fonts/Bally7x9.c', 7, 9)
-
-#spi = SPI(2, baudrate=10000000, sck=Pin(12), mosi=Pin(11))  # Lolin S3 SPI2
-#display = Display(spi, dc=Pin(16), cs=Pin(10), rst=Pin(18))
-# spi = SPI(1, baudrate=10000000, sck=Pin(14), mosi=Pin(13))  # ESP32 SPI1
-# display = Display(spi, dc=Pin(4), cs=Pin(5), rst=Pin(2))
+#I2C interface
 i2c = I2C(0, freq=400000, scl=Pin(5), sda=Pin(4))  # Pico I2C bus1
 display = Display(i2c=i2c, rst=Pin(2))
 
-def test():
+def boot():
 
     display.clear()
     display.draw_rectangle(0, 0, 128, 64)
 
-    display.draw_text(42, 12, "FoxOS", bally)
-    display.draw_text(40, 21, "E.A.R.S" ,bally)
+    display.draw_text(3, 12, "E.A.R.S", bally)
+    display.draw_text(3, 21, "FoxOS" ,bally)
+    display.draw_text(3,30,"Version 1.0.0",bally)
+    display.draw_text(3,39,"Codename: Red",bally)
+    display.present()
+    sleep(3)
+    display.clear()
+    
+def menu():
+    display.draw_rectangle(0, 0, 128, 64)
+    display.draw_text(3,3,"Radio",bally)
+    display.draw_text(3,12,"Weather Report",bally)
+    display.draw_text(3,21,"System Info",bally)
+    display.draw_text(3,30,"Settings",bally)
     display.present()
 
-test()[=
+
+#MAINLOOP
+boot()
+menu()
