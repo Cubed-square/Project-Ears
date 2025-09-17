fa12aa9b4138607cf6ecdb8f36acd9ff77c4a177
Bryce <bryce.schafer@ascte.org>
2025-09-17 13:23:11 -0500

    added pot ability

--- module testing/OLED-testing.py
+++ module testing/OLED-testing.py
@@ -2,6 +2,7 @@
 from time import sleep
 from machine import Pin, I2C  # type: ignore
 from ssd1309 import Display
+from picozero import Button, Pot
 from xglcd_font import XglcdFont
 
 
@@ -10,6 +11,7 @@ bally = XglcdFont('fonts/Bally7x9.c', 7, 9)
 #I2C interface
 i2c = I2C(0, freq=400000, scl=Pin(5), sda=Pin(4))  # Pico I2C bus1
 display = Display(i2c=i2c, rst=Pin(2))
+turner = Pot(27)
 
 def boot():
 
@@ -25,14 +27,41 @@ def boot():
     display.clear()
     
 def menu():
+    global turner
     display.draw_rectangle(0, 0, 128, 64)
-    display.draw_text(3,3,"Radio",bally)
-    display.draw_text(3,12,"Weather Report",bally)
-    display.draw_text(3,21,"System Info",bally)
-    display.draw_text(3,30,"Settings",bally)
+    display.draw_text(3,3,"Radio",bally, invert = False)
+    display.draw_text(3,12,"Weather Report",bally, invert = False)
+    display.draw_text(3,21,"System Info",bally, invert = False)
+    display.draw_text(3,30,"Settings",bally, invert = False)
     display.present()
 
 
 #MAINLOOP
 boot()
 menu()
+while True:
+    if turner.value >= .75 and turner.value <= 1:
+        display.draw_text(3,3,"Radio",bally,invert=True)
+        display.draw_text(3,12,"Weather Report",bally, invert = False)
+        display.draw_text(3,21,"System Info",bally, invert = False)
+        display.draw_text(3,30,"Settings",bally, invert = False)
+        display.present()
+    elif turner.value >= .50 and turner.value <= .74:
+        display.draw_text(3,3,"Radio",bally,invert=False)
+        display.draw_text(3,12,"Weather Report",bally, invert = True)
+        display.draw_text(3,21,"System Info",bally, invert = False)
+        display.draw_text(3,30,"Settings",bally, invert = False)
+        display.present()
+    elif turner.value >= .25 and turner.value <= .49:
+        display.draw_text(3,3,"Radio",bally,invert=False)
+        display.draw_text(3,12,"Weather Report",bally, invert = False)
+        display.draw_text(3,21,"System Info",bally, invert = True)
+        display.draw_text(3,30,"Settings",bally, invert = False)
+        display.present()
+    else:
+        display.draw_text(3,3,"Radio",bally,invert=False)
+        display.draw_text(3,12,"Weather Report",bally, invert = False)
+        display.draw_text(3,21,"System Info",bally, invert = False)
+        display.draw_text(3,30,"Settings",bally, invert = True)
+        display.present()
+    sleep(0.1)
