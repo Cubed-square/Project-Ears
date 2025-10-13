8e013a43388e676f54c0f626df02e6566b64124e
Bryce <bryce.schafer@ascte.org>
2025-10-13 13:25:55 -0500

    Update bme-test.py

--- Required Modules/bme-test.py
+++ Required Modules/bme-test.py
@@ -1,12 +1,6 @@
-from machine import Pin, I2C
-from time import sleep
+import machine
 import bme280
 
-i2c = I2C(0, sda=Pin(8), scl=Pin(9), freq=400000)
-
-print(i2c.scan())
-'''while True:
-    bme = bme280.BME280(i2c=i2c)
-    print(devices = i2c.scan())
-    print(bme.values)
-    sleep(2)'''
+i2c = machine.I2C(sda=machine.Pin(8), scl=machine.Pin(9))
+bme = bme280.BME280(i2c=i2c)
+print(bme.values)
